8a4cd8b21f36ec88c77639577edb342a47ef5cc0
prrat <howto5349@gmail.com>
2021-11-09 07:14:43 +0100

    Working on front end

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import resolvers
 #Pytube pakage for downloading youtube videos
 from pytube import YouTube
 
@@ -10,9 +11,10 @@ def ytb_downloader(request):
     url = request.GET.get('url')
     obj = YouTube(url)
     resolutions = []
-    strm_all = obj.stream.all()
+    strm_all = obj.streams.all()
     for i in strm_all:
-        resolutions.append(i.resolutions)
-    print(resolutions)
+        resolutions.append(i.resolution)
+    resolutions = list(dict.fromkeys(resolutions))
+    print('resolutions: ',resolutions)
 
-    return render(request,'base/download.html')
+    return render(request,'base/download.html',{'rls': resolutions})
